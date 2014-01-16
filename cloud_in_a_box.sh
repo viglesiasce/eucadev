22985ff540ceca028b1b0d8cbe4015e5aaedb48a
Greg DeKoenigsberg <greg@eucalyptus.com>
2014-01-16 01:09:58 -0500

    Add tip of the day to euca console

--- cloud_in_a_box.sh
+++ cloud_in_a_box.sh
@@ -24,3 +24,6 @@ if [ ! -z "$bridge" ];then
     sed -i "s/eth0/$bridge/g" $role.json
 fi
 chef-solo -r cookbooks.tgz -j $role.json
+
+# Add tip of the day to Eucalyptus console
+export UUID=`uuidgen` && sed -i "s|</body>|<iframe width=\"0\" height=\"0\" src=\"https://www.eucalyptus.com/docs/tipoftheday.html?${UUID}\" seamless=\"seamless\" frameborder=\"0\"></iframe></body>|" /usr/share/eucalyptus-console/static/index.html
